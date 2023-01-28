239fb6e64341dd5130f3cbdad04582d393a2ffae
hyacinth-bruno <mbachubruno.chidozie@gmail.com>
2023-01-28 23:27:02 +0100

    Initial commit
    The first commit to demonstrate useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
+// import {axios} from "axios";
+import { useState, useEffect } from "react";
+import Country from "./components/Country";
+
+const url = "https://restcountries.com/v3.1/all";
 
 function App() {
-  return <div className="App"></div>;
+const [data, setData] = useState([]);
+
+async function fetchData() {
+    fetch (url)
+    .then((response) => response.json())
+    .then((data) => setData(data))
+    .catch((error) => console.log(error));
+    console.log(data, "data");
+  }
+
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //let Country = data.slice(0,6);
+  console.log(data, "data");
+
+  return (
+    <div className="App">
+      <h2>Country List</h2>
+
+      {data.slice(0,6).map((item) => {
+        console.log(item);
+          return ( 
+            <Country key={item.id}
+            name={item.name.common}
+            region={item.region}
+            map={item.maps.googleMaps}
+            flag = {item.flag}
+            border = {item.borders}
+            />
+            );
+            
+          })}  
+    </div>
+  );
 }
 
 export default App;
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-export default function Country() {
-  return <div>This is country </div>;
+export default function ProductItems({ name, region, map, flag, border }) {
+  return (
+    <div className="country">
+      <div className="name">Name: {name}</div> <br />
+      <div className="region">region: {region}</div> <br />
+      <a className="map" href={map} target="_blank" rel="noreferrer noopener">
+        Visit map here
+      </a>{" "}
+      <br />
+
+
+      <div className="border">
+  Border:
+  {border ? 
+    border.map((item) => {
+      return <p>{item}</p>;
+    }) : 
+    "no borders"
+  }
+</div>
+
+
+      <div className="flag">flag: {flag}</div>
+    </div>
+  );
 }
